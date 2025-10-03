02b5a359c9cedb9c767cb91e7a1d4a44ee401e56
josepingo12 <josepingofficial@gmail.com>
2025-10-03 23:09:32 +0200

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/studentslife/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  },
 }));
+
